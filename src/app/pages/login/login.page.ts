687fe2426c84c06fef421db6e11704fee18f1e2f
Juan de Santos <juanj.desantos@gmail.com>
2023-10-19 14:03:51 +0200

    first commit

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+  loginContainer: boolean = false;
+
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    const image = this.el.nativeElement.querySelector('.container-intro img');
+    this.renderer.addClass(image, 'logo-transformed');
+    setInterval(() => {
+      this.loginContainer = true;
+    }, 6000);
+  }
+}
